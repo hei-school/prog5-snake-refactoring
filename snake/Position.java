c8ade2663685c4f7bf500422fc0fef3fec736916
Mandresy <hei.mandresy@gmail.com>
2025-12-10 10:25:17 +0300

    feat: refactor snake game

---
+++ snake/Position.java
@@ -0,0 +1,27 @@
+package snake;
+
+public class Position {
+    public final int row;
+    public final int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Position move(Direction direction) {
+        return new Position(row + direction.deltaRow, col + direction.deltaCol);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Position)) return false;
+        Position p = (Position) o;
+        return p.row == row && p.col == col;
+    }
+
+    @Override
+    public int hashCode() {
+        return row * 31 + col;
+    }
+}
